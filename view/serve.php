090d4a92c3763d76da82c96b865b3e3a22063f25
Ana Krmpotić <krmpoticana@gmail.com>
2019-07-07 19:22:14 +0200

    Dodavanje na popis igraca

--- view/serve.php
+++ view/serve.php
@@ -9,22 +9,22 @@ function sendJSONandExit($message)
 }
 
 $error = "";
-$brojIgraca = 'docs/br_igraca.txt';
-// $igraci = 'docs/igraci.txt';
+$fileBrojIgraca = 'docs/br_igraca.txt';
+$fileIgraci = 'docs/igraci.txt';
 
-if(!file_exists($brojIgraca))
-    $error = $error . "File " . $brojIgraca . " doesn't exist!";
-else if(!is_readable($brojIgraca))
-    $error = $error . "File " . $brojIgraca . " is not readable!";
-else if(!is_writable($brojIgraca)){
-    $error = $error . "File " . $brojIgraca - " is not writable!";
+if(!file_exists($fileBrojIgraca))
+    $error = $error . "File " . $fileBrojIgraca . " doesn't exist!";
+else if(!is_readable($fileBrojIgraca))
+    $error = $error . "File " . $fileBrojIgraca . " is not readable!";
+else if(!is_writable($fileBrojIgraca))
+    $error = $error . "File " . $fileBrojIgraca . " is not writable!";
 
-// if(!file_exists($igraci))
-//     $error = $error . "File " . $igraci . " doesn't exist!";
-// else if(!is_readable($igraci))
-//     $error = $error . "File " . $igraci . " is not readable!";
-// else if(!is_writable($igraci))
-//     $error = $error . "File " . $igraci - " is not writable!";
+if(!file_exists($fileIgraci))
+    $error = $error . "File " . $fileIgraci . " doesn't exist!";
+else if(!is_readable($fileIgraci))
+    $error = $error . "File " . $fileIgraci . " is not readable!";
+else if(!is_writable($fileIgraci))
+    $error = $error . "File " . $fileIgraci . " is not writable!";
 
 if($error !== "")
 {
@@ -48,62 +48,56 @@ else if(!isset($_GET['whoSent']))
     sendJSONandExit($response);
 }
 
-else // sve postavljeno pa obradi funkciju
-{
-    if($_GET['whoSent'] === "IWantToJoin") // igrač se želi priključiti, pošalji mu odgovor
-    {
-        $broj_igraca = file_get_contents($brojIgraca);
-        $broj_igraca = intval($broj_igraca);
+if($_GET['whoSent'] === "IWantToJoin") // igrač se želi priključiti, pošalji mu odgovor
+{ 
+    $response = [];
+    $broj_igraca = file_get_contents($fileBrojIgraca);
+    $broj_igraca = intval($broj_igraca);
 
-        $response = [];
-        $response['error'] = "evo ga";
-        if($broj_igraca === 4) // nema slobodnih mjesta pa vrati full
-        {
-            $response['flag'] = "full";
-
-            sendJSONandExit($response);
-        }
-        else // ima slobodnih mjesta pa ga ubaci i vrati indeks zastavice kao signal
-        {
-            $response['flag'] = $broj_igraca;
-            ++$broj_igraca; //povecaj broj igraca i spremi u datoteku
-            file_put_contents($brojIgraca, "" . $broj_igraca);
-
-            sendJSONandExit($response);
-        }
-    }
-
-    else if($_GET['whoSent'] === "CanWeStart") // igrač provjerava može li igra početi
-    {
-        $lastchecked = isset($_GET['timestamp']) ? $_GET['timestamp'] : 0;
-        $currentcheck = filemtime($brojIgraca);
-
-        while($currentcheck <= $lastchecked)
-        {
-            usleep(10000);
-            clearstatcache();
-            $currentcheck = filemtime($brojIgraca);
-        }
-
-        // došao novi igrač
-        $broj_igraca = file_get_contents($brojIgraca);
-        $broj_igraca = intval($broj_igraca);
-        $response['timestamp'] = $currentcheck;
-
-        if($broj_igraca === 4)
-            $response['response'] = "yes";
-        else
-            $response['response'] = "no";
-
-        sendJSONandExit($response);
-    }
-
-    else
+    
+    if($broj_igraca === 4) // nema slobodnih mjesta pa vrati full
     {
         $response['flag'] = "full";
 
         sendJSONandExit($response);
     }
+    else // ima slobodnih mjesta pa ga ubaci i vrati indeks zastavice kao signal
+    {
+        $novi = [];
+        $novi['username'] = $_GET['username'];
+        $novi['indeks'] = $broj_igraca;
+        file_put_contents($fileIgraci, json_encode($novi), FILE_APPEND);
+
+        $response['flag'] = $broj_igraca;
+        ++$broj_igraca; //povecaj broj igraca i spremi u datoteku
+        file_put_contents($fileBrojIgraca, "" . $broj_igraca);
+
+        sendJSONandExit($response);
+    }
+}
+else if($_GET['whoSent'] === "CanWeStart") // igrač provjerava može li igra početi
+{
+    $lastchecked = isset($_GET['timestamp']) ? $_GET['timestamp'] : 0;
+    $currentcheck = filemtime($filename);
+
+    while($currentcheck <= $lastchecked)
+    {
+        usleep(10000);
+        clearstatcache();
+        $currentcheck = filemtime($filename);
+    }
+
+    // došao novi igrač
+    $broj_igraca = file_get_contents($fileBrojIgraca);
+    $broj_igraca = intval($broj_igraca);
+    $response['timestamp'] = $currentcheck;
+
+    if($broj_igraca === 4)
+        $response['response'] = "yes";
+    else
+        $response['response'] = "no";
+
+    sendJSONandExit($response);
 }
 
 ?>
